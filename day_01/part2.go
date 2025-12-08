8fc14ee99858b6eb3c7bbd13c579995b937a37f0
Julia Nemtseva <juliaiskra@gmail.com>
2025-12-08 21:23:39 +0200

    solve part 2

--- day_01/part2.go
+++ day_01/part2.go
@@ -2,7 +2,6 @@ package day_01
 
 import (
 	"bufio"
-	"errors"
 	"os"
 	"strconv"
 )
@@ -25,45 +24,24 @@ func Part2(filename string) (int, error) {
 		if err != nil {
 			return 0, err
 		}
-		var p, zeros int
-		switch direction {
-		case 'L':
-			p, zeros = CountZeros(pointer, -diff)
-		case 'R':
-			p, zeros = CountZeros(pointer, diff)
-		default:
-			return 0, errors.New("invalid direction")
-		}
-		println(pointer, line, p, zeros, zeroCounter)
-		pointer = p
-		zeroCounter += zeros
-		if pointer > 99 || pointer < 0 {
-			return 0, errors.New("invalid pointer")
-		}
-		if pointer == 0 {
-			zeroCounter++
+
+		for i := 0; i < diff; i++ {
+			switch direction {
+			case 'L':
+				pointer--
+				if pointer == -1 {
+					pointer = 99
+				}
+			case 'R':
+				pointer++
+				if pointer == 100 {
+					pointer = 0
+				}
+			}
+			if pointer == 0 {
+				zeroCounter++
+			}
 		}
 	}
-
 	return zeroCounter, nil
-}
-
-func CountZeros(oldPointer int, diff int) (int, int) {
-	newPointer := oldPointer + diff
-	zeroCounter := 0
-	if oldPointer > 0 && newPointer < 0 || oldPointer < 0 && newPointer > 0 {
-		zeroCounter++
-	}
-
-	if newPointer == 100 {
-		return 0, zeroCounter
-	}
-	if newPointer < 0 {
-		mod, _ := CountZeros(100+newPointer%100, 0)
-		return mod, -newPointer/100 + zeroCounter
-	}
-	if newPointer > 99 {
-		return newPointer % 100, newPointer/100 + zeroCounter
-	}
-	return newPointer, 0
 }
--- day_01/part2_test.go
+++ day_01/part2_test.go
@@ -7,12 +7,3 @@ func TestPart2(t *testing.T) {
 	res, _ := Part2("test-input.txt")
 	assert.Equal(t, 6, res)
 }
-
-// This is an example test that intentionally fails.
-//func TestCountZeros(t *testing.T) {
-//	p, zeros := CountZeros(0, 10)
-//	// Intentionally wrong expectation to demonstrate a failing test
-//	if p != 10 || zeros != 1 {
-//		t.Fatalf("Invalid result (intentional fail): got p=%d zeros=%d, want p=10 zeros=1", p, zeros)
-//	}
-//}
